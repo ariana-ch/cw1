aaac59e7d56e494a660f9e1fad8d424b59622c81
arianachristodoulou <ariana.chr@gmail.com>
2025-04-05 15:36:43 +0100

    Won't lose this again

--- CW2/vae_diagonal.py
+++ CW2/vae_diagonal.py
@@ -1,8 +1,8 @@
 from CW2.base_vae import VAE
 from keras.api.metrics import Mean
-from keras import ops
 import numpy as np
-
+import keras
+from keras import ops
 
 
 class VAEDiagonal(VAE):
@@ -12,3 +12,87 @@ class VAEDiagonal(VAE):
         You should override this method as necessary in your implementations.
         """
         super().__init__(encoder=encoder, decoder=decoder, num_mc_samples=num_mc_samples, **kwargs)
+
+
+    def _sample_z(self, z_mean, z_log_var):
+        '''
+        Sample z using the mean and standard deviation output from the decoder and Gaussian
+        noise epsilon ~ N(0, 1) (l dimensional)
+
+            z = mu_q + std_q * epsilon
+        Args:
+            z_mean: The mean of z, obtained using the encoder
+            z_log_var: The log variance of z, obtained using the decoder
+
+        Returns: An (L, l) tensor, where l is the dimension of the latent space and
+        L is the number of Monte Carlo samples required
+        '''
+        batch_size, latent_dim = ops.shape(z_mean)
+
+        # Expand z_mean and z_log_var to shape (L, batch_size, latent_dim)
+        z_mean = ops.expand_dims(z_mean, axis=0)           # shape: (1, B, l)
+        z_log_var = ops.expand_dims(z_log_var, axis=0)     # shape: (1, B, l)
+
+        z_mean = ops.repeat(z_mean, self.L, axis=0)        # shape: (L, B, l)
+        z_log_var = ops.repeat(z_log_var, self.L, axis=0)  # shape: (L, B, l)
+
+        # Sample epsilon ~ N(0, 1)
+        epsilon = keras.random.normal(shape=(self.L, batch_size, latent_dim))
+
+        # Reparameterisation trick
+        z_std = ops.exp(0.5 * z_log_var)
+        z_samples = z_mean + z_std * epsilon  # shape: (L, B, l)
+
+        return z_samples
+
+
+    def compute_losses(self, data):
+        '''
+        Compute the losses. The KL Loss term is the same as the VAE implementation but
+        the reconstruction loss will now be the log likelihood of the Bernoulli distribution
+
+        As with the base class, the encoder outputs the mean and log variance of z.
+        The decoder outputs the probability $p$ of the Bernoulli distribution
+        Args:
+            data: a minibatch of the data
+
+        Returns: the total loss, KL loss and negative log-likelihood loss (scalars)
+        '''
+        # Encode the data
+        z_mean, z_log_var = self.encoder(data)  # shape: (batch, latent_dim)
+
+        # KL divergence (analytical)
+        kl_loss = 0.5 * ops.sum((ops.square(z_mean) + ops.exp(z_log_var) - 1 - z_log_var), axis=-1)
+        kl_loss = ops.mean(kl_loss)  # average over batch
+
+        # Sample L z's per input: shape (L, batch, latent_dim)
+        z_samples = self._sample_z(z_mean=z_mean, z_log_var=z_log_var)
+
+        # Flatten L and batch dims so we can pass them through decoder at once
+        L, B, latent_dim = ops.shape(z_samples)
+        z_samples_flat = ops.reshape(z_samples, (L * B, latent_dim))  # shape: (L*B, latent_dim)
+
+        # Decode: get Bernoulli probs (sigmoid output) for each sample
+        x_recon_flat = self.decoder(z_samples_flat)  # shape: (L*B, D)
+        x_recon = ops.reshape(x_recon_flat, (self.L, -1, ops.shape(x_recon_flat)[-1]))  # shape: (L, B, D)
+
+        # Prepare input x for broadcasting to shape (L, B, D)
+        x = ops.expand_dims(data, axis=0)              # (1, B, D)
+        x = ops.repeat(x, self.L, axis=0)              # (L, B, D)
+
+        # Clip predictions to prevent log(0)
+        eps = 1e-7
+        x = ops.clip(x, eps, 1 - eps)
+        x_recon = ops.clip(x_recon, eps, 1 - eps)
+
+        # Binary cross entropy: shape (L, B)
+        bce = -ops.sum(x * ops.log(x_recon) + (1 - x) * ops.log(1 - x_recon), axis=-1)
+
+        # Mean over L samples and then over batch
+        nll_loss = ops.mean(ops.mean(bce, axis=0))  # average over B, then L
+
+        # Total ELBO loss (negative ELBO)
+        total_loss = kl_loss + nll_loss
+
+        return total_loss, kl_loss, nll_loss
+
